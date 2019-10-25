134a41317bc9a935c9b4a593de64ccc7cd466406
Velhoo NP <als260602@msn.com>
2019-10-25 12:49:28 -0300

    add: corrigindo campo data e hora

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Buttons from "../Buttons";
 import api from "../../services/api";
+
+import {parseISO, format} from 'date-fns'
+
 import "./Dashboard.css";
 
 export default function Dashboard() {
@@ -28,7 +31,9 @@ export default function Dashboard() {
               <button className="btn-li">{fone.codigo}</button>
               <button className="btn-li">{fone.telefone}</button>
               <button className="btn-li">{fone.numero}</button>
-              <button className="btn-li">{`${fone.data} ${fone.hora}`}</button>
+              <button className="btn-li">{
+                format(parseISO(`${fone.data} ${fone.hora}`), "dd/m/y HH:mm")
+                }</button>
             </li>
           ))}
         </ul>
